65322f35bb25049942a8d9bb8733781749672b1b
Alexa Pettitt <21100290@virtualwindow.co.za>
2022-03-09 12:25:41 +0200

    Installed and implemented axios
    Here I added the axio API connection to my numerous component pages.

--- pokemonproject/src/components/Timeline.js
+++ pokemonproject/src/components/Timeline.js
@@ -1,3 +1,13 @@
+import anxios from "axios";
+
+anxios.get('https://pokeapi.co/api/v2')
+.then((response) => { 
+    console.log(response);
+})
+.catch((err) =>{
+    console.log(err);
+});
+
 const Timeline = () => {
 
     return(
