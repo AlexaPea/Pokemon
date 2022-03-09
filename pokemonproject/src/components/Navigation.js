8a00f42d2e05c86d8545693dd48b30b5d6d74dbe
Alexa Pettitt <21100290@virtualwindow.co.za>
2022-03-09 12:20:41 +0200

    Additional Navigation styling
    Here I added a logo and experimented more with the navigation design.

--- pokemonproject/src/components/Navigation.js
+++ pokemonproject/src/components/Navigation.js
@@ -4,11 +4,13 @@ const Navigation = () => {
         <>
             <div className="navContainer">
                 <div className="logoContainer"></div>
+                <h1>Pokemon</h1>
+                <p>Gotta Catch 'Em All</p>
                 
                 <ul>
-                    <li><a href="/Dashboard">Dashboard</a></li>
-                    <li><a href="/Comparisons">Comparisons</a></li>
-                    <li><a href="/Timeline">Timeline</a></li>
+                    <li><div className="icon1"></div><a href="/Dashboard">Dashboard</a></li>
+                    <li><div className="icon2"></div><a href="/Comparisons">Comparisons</a></li>
+                    <li><div className="icon3"></div><a href="/Timeline">Timeline</a></li>
                 </ul>
                 
             </div>
